121523f391a557d901cb9461e54ae4f3c18dd493
t1anist <813400513@qq.com>
2020-04-08 17:25:48 +0800

    Hole cards level classifies. test finished

--- testLevel.py
+++ testLevel.py
@@ -3,27 +3,46 @@ import re
 # test hold_cards_level
 def test():
     count = 0
+    success = 0
     for c1 in range(2, 15):
         for cl1 in range(1, 5):
-            for c2 in range(2, 15):
+            for c2 in range(c1, 15):
                 for cl2 in range(1, 5):
+                    if c1 == c2 and cl1 == cl2:
+                        continue
                     count += 1
                     string = "%d,%d,%d,%d" % (c1, cl1, c2, cl2)
-                    error = 0
                     find = 0
+                    reg = ""
                     for x in level.keys():
-                        print(x)
                         for y in level[x]:
-                            if re.match(y, string):
-                                find = 1
+                            if re.findall(y, string):
+                                find = x
+                                reg = y
                                 break
-                            else:
-                                error = 1
-                        if error:
-                            print("Error, string is:", string)
-    print(count)
+                        if find:
+                            break
+                    if not find:
+                        print("Error: string is ", string)
+                    else:
+                        print(string, "reg is ", reg, "value is ", find)
+                        success += 1
+    print("count = ", count, "success = ", success)
+
+def test1(string):
+    find = 0
+    for x in level.keys():
+        for y in level[x]:
+            if re.findall(y, string):
+                find = 1
+                print("reg is ", y, "value is ", x)
+                break
+        if find:
+            break
+
 
 
 
 if __name__ == '__main__':
     test()
+    test1("14,1,14,2")
